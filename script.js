d927dfd784ed103ef50136a023bb873560a1971a
JhoanV06 <jhoan.santiago111@gmail.com>
2025-05-29 17:27:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const messages = [
+    "Te quiero mucho",
+    "Gracias por estar en mi vida💯",
+    "Hola mi estrellita✨",
+    "Eres mi persona favorita✨",
+    "Ya te dije que eres muy linda?🛐",
+    "Me encantan tus ojitos😮‍💨",
+    "Me encanta estar contigo🌹",
+    "No dejo de pensar en ti💫",
+    "Mi corazón ya es tuyo🫶",
+    "Me fascina tu forma de ser💯"
+]
+
+function createTextBubble() {
+    const bubble = document.createElement("div");
+    bubble.className = "text-bubble";
+    bubble.innerText = messages[Math.floor(Math.random() * messages.length)];
+
+    const left = Math.random() * 80 + 10;
+    const top = Math.rando() * 80 + 10;
+
+    bubble.style.position = "absolute";
+    bubble.style.left = left + "vw";
+    bubble.style.top = top + "vh"
+
+    const container = document.getElementById("bubbles-text");
+    container.appendChild(bubble);
+
+    setTimeout(() => {
+        const rect = bubble.getBoundingClientRect();
+
+        if (rect.right > window.innerWidth) {
+            const newLeft = window.innerWidth - rect.width - 10;
+            bubble.style.left = `${newLeft}px`;
+        }
+
+        if (rect.bottom > window.innerHeight) {
+            const newTop = window.innerHeight - rect.height - 10;
+            bubble.style.top = `${newTop}px`;
+        }
+
+        if (rect.left < 0) {
+            bubble.style.left = "10px";
+        }
+
+        if (rect.top < 0) {
+            bubble.style.top = "10px"
+        }
+    }, 10);
+
+    setTimeout(() => {
+        bubble.remove();
+    }, 8000);
+}
+
+setInterval(createTextBubble, 500 )
